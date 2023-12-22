da3384accc6d91d1a81d3989a1e5bbe55aa5a423
LuXuAn <5001363@qq.com>
2023-12-22 22:56:33 +0800

    复习

--- 数据结构/数据结构/期末复习.cpp
+++ 数据结构/数据结构/期末复习.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 int main()
 {
-    int* p = new int;
-    cout << p;
+	//初始化一维数组以及二维数组的注意事项和格式
+	int* p = new int[5];
+	//初始化时同时赋初值
+	int* p2 = new int(3);
+	cout << *p2 << endl;
+	//cout << "一维数组的创建" << endl;
+	int(*p1)[3] = new int[1][3];
+	//cout << "要注意初始化的时候要与列数一致" << endl;
+	cout << sizeof(p);
+
+
 }
