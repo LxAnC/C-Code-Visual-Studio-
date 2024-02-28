a5cb36d1a7465115c79a901730f2f99006582c31
LuXuAn <5001363@qq.com>
2024-02-28 22:36:49 +0800

    update

--- 算法设计与分析/实验一希尔排序.cpp
+++ 算法设计与分析/实验一希尔排序.cpp
@@ -1,28 +1,33 @@
+//Ï£¶ûÅÅÐò
 #include<iostream>
 #include<algorithm>
 int i, o;
 using namespace std;
 int main()
 {
-	int a[11];
-	int x,i=0;
-	while (cin >> a[i++]) {
-		if (i == 10)
+	int a[1000],s=0,n,i,d,j,ans;
+	while (cin >> a[s])
+	{
+		s++;
+		if (cin.get() == '\n')
 			break;
 	}
-	x = i;
-	cin >> a[10];
-	while (1) {
-		x = x / 2;
-		for (i = 0; i < x; i++)
+	n = s;
+	while (1)
+	{
+		n /= 2;
+		for (i = 0; i < n; i++)
 		{
-			i = a[i];
-			o = a[i + x ];
-			a[i] = max(i,o );
-			a[i + x - 1] = min(i, o);
+			for (j = i + n; j < s; j += n){
+				ans = a[j];
+			for (d = j - n; d >= 0 && a[d] < ans; d -= n)
+				a[d + n] = a[d]; 
+			a[d + n] = ans;}
 		}
-		if (x == 1)
+		if (n == 1)
 			break;
-
-	}
+}
+    for (i = 0; i < s; i++)
+        cout << a[i] << " ";
+	
 }
