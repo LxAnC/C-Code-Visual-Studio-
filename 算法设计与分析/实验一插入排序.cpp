7f8c3b99430694f4439dde66501232194f1af2be
LuXuAn <5001363@qq.com>
2024-02-28 18:08:10 +0800

    update

--- 算法设计与分析/实验一插入排序.cpp
+++ 算法设计与分析/实验一插入排序.cpp
@@ -1,13 +1,28 @@
 #include<iostream>
 using namespace std;
-int i = 0;
+int s = 0,b,loc,i;
 int main()
 {
-	int a[1000];
-	while (cin >> a[i])
-	{
-		i++;
-		if (cin.get() == '\n')
+	int a[1000],c=1;
+		while (cin >> a[s]) {
+		s++;
+		if (cin.get() == '\n') {
 			break;
+		}
 	}
+	while (c <= s)
+	{
+		b = a[c];
+		for (i = 0; i < c; i++)
+			if (b <= a[i])
+				loc = i;
+		if (b > a[i - 1])
+			loc = c;
+		if(b == a[loc])
+		for (int j = s - 1; j >= c; j--)
+			a[j + 1] = a[j];
+		a[c] = b; c++;
+	}
+	for (i = 0; i < s - 1; i++)
+		cout << a[i] << " ";
 }
