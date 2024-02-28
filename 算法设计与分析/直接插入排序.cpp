08a3b43306a0c042dd969314d4ff2b4679571c5a
LuXuAn <5001363@qq.com>
2024-02-28 19:39:53 +0800

    学习直接插入排序,原理就是从第二个元素开始,进行对比,如果小于或者大于就先把这个数存储起来,然后将比他大的数向后移动,最后那个位置就是他需要插入的位置

---
+++ 算法设计与分析/直接插入排序.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[1000];
+	int s = 0,i,j,temp;
+	while (cin >> a[s])
+		{
+			s++;
+			if (cin.get() == '\n')
+				break;
+	}
+	for (i = 1; i < s; i++)
+	{
+		if (a[i] < a[i - 1])
+		{
+			temp = a[i];
+			for (j = i - 1; j >= 0 && a[j] > temp; j--)
+				a[j + 1] = a[j];
+			a[j + 1] = temp;
+		}
+	}
+	for (i = 0; i < s; i++)
+		cout << a[i] << " ";
+}
