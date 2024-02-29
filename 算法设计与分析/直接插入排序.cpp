67cf022ac4302f98d67fb727e37f3219e1ffa3f8
LuXuAn <5001363@qq.com>
2024-02-29 18:40:15 +0800

    复习了两种排序算法,分别是希尔排序和直接插入排序有异曲同工之妙

--- 算法设计与分析/直接插入排序.cpp
+++ 算法设计与分析/直接插入排序.cpp
@@ -1,24 +1,48 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int a[1000];
+//	int s = 0,i,j,temp;
+//	while (cin >> a[s])
+//		{
+//			s++;
+//			if (cin.get() == '\n')
+//				break;
+//	}
+//	for (i = 1; i < s; i++)
+//	{
+//		if (a[i] < a[i - 1])
+//		{
+//			temp = a[i];
+//			for (j = i - 1; j >= 0 && a[j] > temp; j--)
+//				a[j + 1] = a[j];
+//			a[j + 1] = temp;
+//		}
+//	}
+//	for (i = 0; i < s; i++)
+//		cout << a[i] << " ";
+//}
 #include<iostream>
 using namespace std;
 int main()
 {
-	int a[1000];
-	int s = 0,i,j,temp;
+	int a[100],i,j,temp;
+	int s = 0;
 	while (cin >> a[s])
-		{
-			s++;
-			if (cin.get() == '\n')
-				break;
+	{
+		s++;
+		if (cin.get() == '\n')
+			break;
 	}
 	for (i = 1; i < s; i++)
 	{
-		if (a[i] < a[i - 1])
+		temp = a[i];
+		for (j = i - 1; j >= 0 && a[j] < temp; j--)
 		{
-			temp = a[i];
-			for (j = i - 1; j >= 0 && a[j] > temp; j--)
-				a[j + 1] = a[j];
-			a[j + 1] = temp;
+			a[j + 1] = a[j];
 		}
+		a[j + 1] = temp;
 	}
 	for (i = 0; i < s; i++)
 		cout << a[i] << " ";
